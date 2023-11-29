2901f1d6626492d68e3d8c40d9cb18856aea8c6d
ctpanh <ctpanh03@gmail.com>
2023-11-29 07:51:30 +0700

    feat: store user

---
+++ client/src/stores/authStore.ts
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+
+interface authState {
+  authorized: boolean;
+  setAuthorized: (authorized: boolean) => void;
+  token: string;
+  setToken: (token: string) => void;
+  removeToken: () => void;
+  logout: () => boolean;
+}
+
+const useAuthStore = create<authState>((set) => ({
+  authorized: false,
+  setAuthorized: (authorized) => set(() => ({ authorized })),
+  token: "",
+  setToken: (token) => set(() => ({ token })),
+  removeToken: () => set({ token: "" }),
+  logout: () => {
+    // Logout for axios
+    const token = localStorage.getItem("token");
+    localStorage.removeItem("token");
+    set({ token: "" });
+    return !!token;
+  },
+}));
+
+export default useAuthStore;
